cfe276757721037df21fef839cfb6b60c17f103b
Oh Ji Won <ozy9665@gmail.com>
2025-02-20 23:02:31 +0900

    제단 충돌처리

--- Cult/Source/Cult/Altar.cpp
+++ Cult/Source/Cult/Altar.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Altar.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 #include "CultistCharacter.h"
 
 // Sets default values
@@ -10,11 +12,20 @@ AAltar::AAltar()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
-	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
-	RootComponent = MeshComponent;
+	// 제단 메쉬
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
 
-	// 
-	OnActorBeginOverlap.AddDynamic(this, &AAltar::OnBeginOverlap);
+	// 콜리전 생성
+	CollisionComp = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionComp"));
+	CollisionComp->SetupAttachment(MeshComp);
+	CollisionComp->SetBoxExtent(FVector(100.0f, 100.0f, 100.0f));
+
+	// 충돌 이벤트 바인드
+	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &AAltar::OnOverlapBegin);
+	CollisionComp->OnComponentEndOverlap.AddDynamic(this, &AAltar::OnOverlapEnd);
+
+	bPlayerInRange = false;
 }
 
 // Called when the game starts or when spawned
@@ -24,12 +35,24 @@ void AAltar::BeginPlay()
 	
 }
 
-void AAltar::OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+void AAltar::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	ACultistCharacter* Cultist = Cast<ACultistCharacter>(OtherActor);
-	if (Cultist)
+	
+	if (ACultistCharacter* Cultist = Cast<ACultistCharacter>(OtherActor))
 	{
-		Cultist->CurrentAltar = this;
+		bPlayerInRange = true;
+	}
+}
+
+void AAltar::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+	// 떠날 때
+	if (ACultistCharacter* Cultist = Cast<ACultistCharacter>(OtherActor))
+	{
+		bPlayerInRange = false;
 	}
 }
 
--- Cult/Source/Cult/Altar.h
+++ Cult/Source/Cult/Altar.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "Altar.generated.h"
 
+
 UCLASS()
 class CULT_API AAltar : public AActor
 {
@@ -19,13 +20,42 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
-	class UStaticMeshComponent* MeshComponent;
 
 public:	
-	UFUNCTION()
-	void OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	// Property
 	
+	// 메쉬
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UStaticMeshComponent* MeshComp;
+
+	// 콜리전
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UBoxComponent* CollisionComp;
+
+	// 영역 안 체크
+	bool bPlayerInRange;
+	
+	
+
+	// 충돌처리
+ 
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
+	// OvelappedComp => 콜리전 컴포넌트 => 이 이벤트를 발생 => 제단의 콜리전박스
+	// OtherActor : 충돌한 다른 액터 (cast를 통하여 Cultist만 감지)
+	// OtherComp : 아직은 활용x . 지금은 Cultist와의 충돌만
+	// bFromSweep : 이동중인지
+	// SweepResult : 충돌정보(위치,벡터 등)
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
