5fca8482840a98cd408e2e6a8033df3d172c30e7
Oh Ji Won <ozy9665@gmail.com>
2025-03-01 22:49:58 +0900

    발사체 코드 및 컨트롤러 코드 작성(오류 수정중), 프로젝트 세팅 입력_축 수정
    GetFirst 를 통한 빙의로 추정,
    엔딩적용 오류 발견
    Pistol의 발사체 준비 및 움직임 프로젝트세팅 수정

---
+++ Cult/Source/Cult/Bullet.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bullet.h"
+
+// Sets default values
+ABullet::ABullet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void ABullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	AActor* MyOwner = GetOwner();
+	if (MyOwner == nullptr)
+	{
+		Destroy();
+		return;
+	}
+
+
+
+
+
+
+	// ÆÄ±«
+	Destroy();
+}
---
+++ Cult/Source/Cult/CultPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CultPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CULT_API ACultPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
