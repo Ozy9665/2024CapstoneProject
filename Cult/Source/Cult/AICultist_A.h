3e2221cf9a0e3457c69f815ddb306030f3cd4cf1
Oh Ji Won <ozy9665@gmail.com>
2025-04-16 21:22:11 +0900

    CultistAI별도 생성, GameMode생성

---
+++ Cult/Source/Cult/AICultist_A.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Cultist_A.h"
+#include "AICultist_A.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CULT_API AAICultist_A : public ACultist_A
+{
+	GENERATED_BODY()
+	
+};
---
+++ Cult/Source/Cult/BlackGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlackGameModeBase.h"
+
+ABlackGameModeBase::ABlackGameModeBase()
+{
+	DefaultPawnClass = nullptr;
+	PlayerStateClass = nullptr;
+	HUDClass = nullptr;
+}
