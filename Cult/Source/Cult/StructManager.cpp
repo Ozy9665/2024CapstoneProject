367a638f12cba0dfb67db16af7b88c965a0202bd
Oh Ji Won <ozy9665@gmail.com>
2026-01-27 20:51:48 +0900

    계획 및 테스트환경

--- Cult/Source/Cult/StructManager.cpp
+++ Cult/Source/Cult/StructManager.cpp
@@ -1,27 +1,100 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "StructManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
 
-// Sets default values
 AStructManager::AStructManager()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
+	PrimaryActorTick.bCanEverTick = false; 
 }
 
-// Called when the game starts or when spawned
 void AStructManager::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	BuildBaseColumns();
+	LinkNeighborsByX();
+	DrawDebug();
 }
 
-// Called every frame
-void AStructManager::Tick(float DeltaTime)
+void AStructManager::BuildBaseColumns()
 {
-	Super::Tick(DeltaTime);
+	TArray<AActor*> Found;
+	UGameplayStatics::GetAllActorsWithTag(GetWorld(), ColumnTag, Found);
 
+	Nodes.Empty();
+
+	for (AActor* A : Found)
+	{
+		if (!IsValid(A)) continue;
+
+		FVector Origin, Extent;
+		A->GetActorBounds(true, Origin, Extent);
+
+		const float MinZ = Origin.Z - Extent.Z;
+
+		// 바닥에 닿는 기둥 -> Node
+		if (MinZ <= BaseColumnMinZThreshold)
+		{
+			FStructNode N;
+			N.Actor = A;
+			N.NodeId = Nodes.Num();
+
+			// Capacity 우선 동일하게 시작 
+			N.CurrentLoad = 1.0f;
+			N.Capacity = 3.0f;
+
+			Nodes.Add(N);
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("[StructManager] Found Columns=%d, BaseColumns=%d"),
+		Found.Num(), Nodes.Num());
 }
 
+void AStructManager::LinkNeighborsByX()
+{
+	// X축으로 정렬
+	Nodes.Sort([](const FStructNode& A, const FStructNode& B)
+		{
+			if (!IsValid(A.Actor) || !IsValid(B.Actor)) return false;
+			return A.Actor->GetActorLocation().X < B.Actor->GetActorLocation().X;
+		});
+
+	// 정렬 후 NodeId 재부여 -> 정렬순서 ID
+	for (int32 i = 0; i < Nodes.Num(); ++i)
+	{
+		Nodes[i].NodeId = i;
+		Nodes[i].LeftId = (i > 0) ? (i - 1) : -1;
+		Nodes[i].RightId = (i < Nodes.Num() - 1) ? (i + 1) : -1;
+	}
+}
+
+void AStructManager::DrawDebug()
+{
+	if (!bDrawDebugGraph) return;
+
+	for (int32 i = 0; i < Nodes.Num(); ++i)
+	{
+		AActor* A = Nodes[i].Actor;
+		if (!IsValid(A)) continue;
+
+		const FVector P0 = A->GetActorLocation();
+
+		// 노드
+		const FString Txt = FString::Printf(TEXT("ID:%d  L:%.1f/%.1f"),
+			Nodes[i].NodeId, Nodes[i].CurrentLoad, Nodes[i].Capacity);
+
+		DrawDebugString(GetWorld(), P0 + FVector(0, 0, 120), Txt, nullptr, FColor::Cyan, DebugDuration);
+
+		// 오른쪽 이웃 연결선
+		if (Nodes[i].RightId != -1 && Nodes.IsValidIndex(Nodes[i].RightId))
+		{
+			AActor* B = Nodes[Nodes[i].RightId].Actor;
+			if (IsValid(B))
+			{
+				const FVector P1 = B->GetActorLocation();
+				DrawDebugLine(GetWorld(), P0, P1, FColor::Green, false, DebugDuration, 0, DebugLineThickness);
+			}
+		} 
+	}
+}
--- Cult/Source/Cult/StructManager.h
+++ Cult/Source/Cult/StructManager.h
@@ -6,21 +6,54 @@
 #include "GameFramework/Actor.h"
 #include "StructManager.generated.h"
 
+USTRUCT()
+struct FStructNode
+{
+	GENERATED_BODY()
+
+	UPROPERTY() AActor* Actor = nullptr;
+	UPROPERTY() int32 NodeId = -1;
+
+	// 그래프
+	UPROPERTY() int32 LeftId = -1;
+	UPROPERTY() int32 RightId = -1;
+
+	// 하중
+	UPROPERTY() float CurrentLoad = 1.0f;
+	UPROPERTY() float Capacity = 3.0f;
+};
+
 UCLASS()
 class CULT_API AStructManager : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
+
+public:
 	AStructManager();
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	UPROPERTY(EditAnywhere, Category = "Struct|Tags")
+	FName ColumnTag = "Struct_Column";
 
+	// 바닥 기둥 판정 임계값  / 최대높이
+	UPROPERTY(EditAnywhere, Category = "Struct|Filter")
+	float BaseColumnMinZThreshold = 5.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Struct|Debug")
+	bool bDrawDebugGraph = true;
+
+	UPROPERTY(EditAnywhere, Category = "Struct|Debug")
+	float DebugDuration = 0.0f; //
+
+	UPROPERTY(EditAnywhere, Category = "Struct|Debug")
+	float DebugLineThickness = 4.0f;
+
+private:
+	UPROPERTY() TArray<FStructNode> Nodes;
+
+	void BuildBaseColumns();
+	void LinkNeighborsByX();
+	void DrawDebug();
 };
